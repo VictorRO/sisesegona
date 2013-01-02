f51bbeb3eff6d76789c962efcbdde49f0fab4d5e
Victor Rodriguez <victor@v-code.org>
2013-01-02 09:56:45 +0100

    dynamize colors to the blog name with jquery

--- js/main.js
+++ js/main.js
@@ -13,31 +13,21 @@
 	var extensionCollection = Extension.collection;
 	
 	/**
-	 * Find the H1 a tag and add <span class="dark" /> at the half of the title
+	 * Find the H1 a tag and add <span class="dark" /> at the second half of the blog name
 	 **/
 	extensionCollection.linkDarkSpan = function (scope) {
-		var $h1_link = $('#site-name', scope);
+		var $siteName = $('#site-name', scope);
 		
-		if (!$h1_link.length) return;
+		if (!$siteName.length) return;
 		
-		$h1_link.each(function () {
-			var str = $(this).text();
-			if( str.indexOf(' ') > 0 ) { var space = ' '; } 
-			else { var space = ''; }
-			var strArray = str.split(space),
-			fullLength = strArray.length,
-			halfLength = Math.ceil( fullLength / 2 ),
-			restLength = fullLength - halfLength,
-			newstr = '';
-			for( var i = 0; i < halfLength; i++ ) {
-				newstr += strArray[i] + space;
-			}
-			newstr += '<span class="dark">';
-			for( var i = halfLength; i < fullLength; i++ ) {
-				newstr += strArray[i] + space;
-			}
-			newstr += '</span>';
-			$h1_link.html(newstr);
+		$siteName.each(function () {
+			var name = $(this).text();
+			var separator = (name.indexOf(' ') > 0) ? ' ' : '';
+			var nameArray = name.split(separator),
+				halfLength = Math.ceil(nameArray.length / 2);	
+			nameArray.splice(halfLength, 0, '<span class="dark">');
+			nameArray.push('</span>');
+			$(this).html(nameArray.join(separator));
 		});
 	};
 	
