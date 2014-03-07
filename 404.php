6d97b0224bd3fa711e20b3a0238621d5b77239a0
Victor Rodriguez <victor@v-code.org>
2014-03-07 10:59:04 +0100

    Added authors to the side bar

--- 404.php
+++ 404.php
@@ -1,20 +1,22 @@
 <?php get_header();?>
-<section>
-  <article>
-    <hgroup>
-      <p>
-        <?php echo date('j F, Y'); ?>
-      </p>
-      <h2 class="title">404 - The Server can not find it !</h2>
-    </hgroup>
-    <div class="entry">
-      <p>El artículo o página que estás buscando no está disponible.
-        Puede que haya sido borrado o haya cambiado su URL.</p>
-      <p>Por favor busca en el archivo / busca por el site.</p>
-    </div>
-    <div class="comments">
-      <p>Escrito como "Not Found"</p>
-    </div>	          
-  </article>      
-</section>
-<?php get_footer();?>
+<div id="wrapper" class="clearfix">
+	<section class="clearfix">
+	  <article>
+	    <hgroup>
+	      <p>
+	        <?php echo date('j F, Y'); ?>
+	      </p>
+	      <h2 class="title">404 - The Server can not find it !</h2>
+	    </hgroup>
+	    <div class="entry">
+	      <p>El artículo o página que estás buscando no está disponible.
+	        Puede que haya sido borrado o haya cambiado su URL.</p>
+	      <p>Por favor busca en el archivo / busca por el site.</p>
+	    </div>
+	    <div class="comments">
+	      <p>Escrito como "Not Found"</p>
+	    </div>	          
+	  </article>      
+	</section>
+	<?php get_footer();?>
+</div>
