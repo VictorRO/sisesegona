cce4d7ffbca5521d7a4d0096b792c79405c2ec8e
Victor Rodriguez <victor@rodriguezortega.com>
2016-05-25 10:17:01 +0200

    some bug fixes and improve the authors views

--- author-david.php
+++ author-david.php
@@ -1,22 +1,20 @@
 <aside id="author">
-
 	<?php
 	$author = get_userdata(get_the_author_meta('ID'));
 	?>
 	<div class="author-meta clearfix">
 		<?php echo get_avatar( $author->user_email, $size = '64' ); ?>
-		<!-- <h1>The author</h1> -->
 		<h1><?php echo $author->first_name . ' ' . $author->last_name; ?></h1>
+		<h2><?php echo $author->user_description; ?></h2>
+		<ul>
+			<li><i class="fi-mail"></i> <?php echo the_author_meta('user_email') ?></li>
+		</ul>
 	</div>
 
-	<div class="small">
-		<p><?php echo $author->user_description; ?></p>
-	</div>
-
-  <ul class="social">
-  	<li><a href="http://es.linkedin.com/in/davidrodriguezortega/" rel="author" target="_blank" class="fi-social-linkedin"> <span class="small">linkedin.com/in/davidro...</span></a></li>
-  	<li><a href="https://github.com/dyunk" target="_blank" class="fi-social-github"> <span class="small">github.com/dyunk</span></a></li>
-  	<li><a href="http://rodriguezortega.com" class="fi-torso"> <span class="small">rodriguezortega.com</span></a></li>
+	<ul class="social">
+  	<li><a href="http://es.linkedin.com/in/davidrodriguezortega/" rel="author" target="_blank" class="fi-social-linkedin"></a></li>
+  	<li><a href="https://github.com/dyunk" target="_blank" class="fi-social-github"></a></li>
+  	<li><a href="http://rodriguezortega.com" class="fi-torso"></a></li>
   </ul>
 
 </aside>
--- author-victor.php
+++ author-victor.php
@@ -1,23 +1,22 @@
 <aside id="author">
-
 	<?php
 	$author = get_userdata(get_the_author_meta('ID'));
 	?>
 	<div class="author-meta clearfix">
 		<?php echo get_avatar( $author->user_email, $size = '64' ); ?>
-		<!-- <h1>The author</h1> -->
 		<h1><?php echo $author->first_name . ' ' . $author->last_name; ?></h1>
-	</div>
-
-	<div class="small">
-		<p><?php echo $author->user_description; ?></p>
+		<h2><?php echo $author->user_description; ?></h2>
+		<ul>
+			<li><i class="fi-mail"></i> <?php echo the_author_meta('user_email') ?></li>
+			<li><i class="fi-telephone"></i> <?php echo the_author_meta('aim'); ?></li>
+		</ul>
 	</div>
 
   <ul class="social">
-  	<li><a href="https://twitter.com/_victorrO_" rel="author" target="_blank" class="fi-social-twitter"> <span class="small">twitter.com/_victorro_</span></a></li>
-  	<li><a href="http://es.linkedin.com/in/victorrodriguezortega" rel="author" target="_blank" class="fi-social-linkedin"> <span class="small">linkedin.com/in/victorro...</span></a></li>
-  	<li><a href="https://github.com/VictorRO" target="_blank" class="fi-social-github"> <span class="small">github.com/VictorRO</span></a></li>
-  	<li><a href="http://rodriguezortega.com" class="fi-torso"> <span class="small">rodriguezortega.com</span></a></li>
+  	<li><a href="https://twitter.com/_victorrO_" rel="author" target="_blank" class="fi-social-twitter"></a></li>
+  	<li><a href="http://es.linkedin.com/in/victorrodriguezortega" rel="author" target="_blank" class="fi-social-linkedin"></a></li>
+  	<li><a href="https://github.com/VictorRO" target="_blank" class="fi-social-github"></a></li>
+  	<li><a href="http://rodriguezortega.com" class="fi-torso"></a></li>
   </ul>
 
 </aside>
